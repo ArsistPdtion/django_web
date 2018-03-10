109c3ebe28797d4d0ffcf77c502b604a86f908f2
Arsist <15711026959@163.com>
2018-03-10 16:07:16 +0800

    add some config for deploy web

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1#=2$!risdc$6n*oip))p!5dzl8!zn2xmd-b!$4p2qy^m#^rk0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','120.79.171.225','localhost','.tanglong666.top']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
